10f354206d86c472e217013a2a9a417b43bc15a2
wekesa931 <wekesabill@gmail.com>
2021-03-29 21:35:30 +0300

    deployment

--- config/urls.py
+++ config/urls.py
@@ -23,15 +23,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
-
-# from django.conf.urls import url
-# from django.contrib import admin
-# from django.views.decorators.csrf import csrf_exempt
-# from .schema import schema
-
-# from graphene_django.views import GraphQLView
-
-# urlpatterns = [
-#     url(r'^admin/', admin.site.urls),
-#     url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
-# ]
